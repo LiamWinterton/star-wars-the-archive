70b3f85165b5b4dd6cd7a8932b34513d4e3ecdb1
Liam <liam.winterton.dev@gmail.com>
2022-01-30 20:53:45 +0000

    Created a Relative component, LOTS of css stuff

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,5 +1,9 @@
 import Image from 'next/image'
+
 import Menu from '../Menu/Menu'
+import Relative from '../Relative/Relative'
+
+import styles from './Header.module.sass'
 
 export default function Header() {
 	const links = [
@@ -9,13 +13,21 @@ export default function Header() {
 		{ text: "Planets", href: "/planets" }
 	]
 
-	return (
-		<header className="header">
+	const content = (
+		<div className="container">
 			<div className="logo">
-				<Image src={"/death-star.svg"} alt="Star Wars: The Archive" width="100px" height="100px" />
+				<Image src={"/death-star.svg"} alt="Star Wars: The Archive" width={100} height={100} />
 			</div>
 
 			<Menu links={links} />
-		</header>
+		</div>
+	)
+
+	return (
+		<Relative 
+			wrapper={<header className={styles.header}></header>}
+			content={content}
+			backgroundImage={<Image src={"/space.png"} objectFit="cover" layout="fill" alt="" />}
+		/>
 	)
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/reset.css"
+import "../styles/global.css"
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />
