393968b161954ef3ffe1875277f2081b09d6a479
Liam <liam.winterton.dev@gmail.com>
2022-01-30 23:34:37 +0000

    I think we have a working header bro!

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -1,12 +1,12 @@
-import Link from "next/link"
-
 import MenuItem from "./MenuItem"
+
+import styles from './Menu.module.sass'
 
 export default function Menu(props) {
 	const { links } = props
 
 	return (
-		<div className="menu">
+		<div className={styles.menu}>
 			{links.map((link, i) => {
 				return <MenuItem key={link.href} href={link.href} text={link.text} />
 			})}
--- components/Menu/MenuItem.js
+++ components/Menu/MenuItem.js
@@ -1,9 +1,13 @@
 import Link from "next/link"
 
+import styles from './MenuItem.module.sass'
+
 export default function MenuItem(props) {
 	const { text, href } = props
 
 	return (
-		<Link href={href}>{text}</Link>
+		<Link href={href}>
+			<a className={styles.link}>{text}</a>
+		</Link>
 	)
 }
