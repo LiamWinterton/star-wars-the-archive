76643ed65b00be38b45777ca6ac10c80f18303ea
Liam <liam.winterton.dev@gmail.com>
2022-01-30 23:19:09 +0000

    Added some hero text and a new font

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,6 +4,7 @@ import styles from "./Hero.module.sass"
 export default function Hero() {
 	return (
 		<div className={styles.hero}>
+			<h1 className={styles.text}>Star Wars:<br />The Archive</h1>
 			<Image src="/hero-luke-2.png" alt="Luke Skywalker" width={600} height={600} />	
 		</div>
 	)
