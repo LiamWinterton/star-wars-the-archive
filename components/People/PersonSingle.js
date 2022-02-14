7bdb87751ffc34f8713588cd8e1c36fcbdd86eae
Liam <liam.winterton.dev@gmail.com>
2022-02-14 20:14:22 +0000

    Link to an individual person page

--- components/People/PersonSingle.js
+++ components/People/PersonSingle.js
@@ -3,8 +3,13 @@ import Link from "next/link"
 export default function PersonSingle(props) {
 	const { person } = props
 
+	const regex = /\/people\/(\d+)\//
+	const result = regex.exec(person.url)
+
+	const id = parseInt(result[1])
+
 	return (
-		<Link href="">
+		<Link href={`/person/${id}`}>
 			<a>{person.name}</a>
 		</Link>
 	)
