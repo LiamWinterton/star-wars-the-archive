8fbb1222e9421d5659226de8090309f5deb8b9e9
Liam <liam.winterton.dev@gmail.com>
2022-02-23 20:52:57 +0000

    Center homepage section

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
 					<p>Cursus augue tincidunt blandit leo porttitor nunc etiam condimentum morbi. Posuere consectetur pretium malesuada sem nulla ultricies nunc. Maecenas gravida tortor egestas sollicitudin diam. Erat cursus lorem facilisi non, magnis in. Fringilla proin magna proin aliquet pellentesque eu hac eget ut. Ultricies amet mauris in aliquet fames dolor sit. Malesuada dictumst in ac, velit elit. Amet sit gravida non sapien, nulla a lacus viverra erat. Rhoncus laoreet aliquet facilisis pharetra tortor.</p>
 				</Section>
 
-				<Section title="upcoming features">
+				<Section title="upcoming features" type="center">
 					<Features />
 				</Section>
 			</Layout>
