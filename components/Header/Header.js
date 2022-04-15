79d9375c50434adf25e7fd08a64856e094a2e174
Liam <liam.winterton.dev@gmail.com>
2022-04-15 18:00:57 +0100

    Start working on mobile header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 
 import Hero from '../Hero/Hero'
 import Menu from '../Menu/Menu'
-import Relative from '../Relative/Relative'
 
 import Link from 'next/link'
 
@@ -17,27 +16,22 @@ export default function Header() {
 		{ text: "Planets", href: "/planets/1" }
 	]
 
-	const content = (
-		<div className="container">
-			<div>
-				<div className="logo">
-					<Link href="/" passHref>
-						<a>
-							<Image src={"/death-star.svg"} alt="Star Wars: The Archive" width={100} height={100} />
-						</a>
-					</Link>
-				</div>
-				<Menu links={links} layout="horizontal" />
-			</div>
-			{router.pathname === "/" ? <Hero /> : undefined}
-		</div>
-	)
-
 	return (
-		<Relative 
-			wrapper={<header></header>}
-			content={content}
-			backgroundImage={<Image src={"/space.jpg"} objectFit="cover" layout="fill" alt="" priority />}
-		/>
+		<header className="relative">
+			<div className="container relative z-20">
+				<div>
+					<div>
+						<Link href="/" passHref>
+							<a>
+								<Image src={"/death-star.svg"} alt="Star Wars: The Archive" width={100} height={100} />
+							</a>
+						</Link>
+					</div>
+					<Menu links={links} layout="horizontal" />
+				</div>
+				{/* {router.pathname === "/" ? <Hero /> : undefined} */}
+			</div>
+			<Image src={"/space.jpg"} objectFit="cover" layout="fill" alt="" className="z-10" priority />
+		</header>
 	)
 }
