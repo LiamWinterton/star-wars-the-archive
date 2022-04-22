d881e60988f7a7e8497f904c4a278bd48305c390
Liam <liam.winterton.dev@gmail.com>
2022-04-22 19:38:40 +0100

    Nav actually clickable now

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Menu({ links }) {
 	return (
-		<nav>
+		<nav className="relative z-50">
 			<ul className="md:flex md:space-x-12 text-white font-bold lg:text-md xl:text-2xl">
 				{links.map((link, i) => {
 					return (
