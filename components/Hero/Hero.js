942c77422c29b39a9949a5ae98ad7a21b40598ba
Liam <liam.winterton.dev@gmail.com>
2022-04-22 18:33:42 +0100

    Desktop hero styling!

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function Hero() {
 	return (
-		<div className="flex relative md:py-40">
+		<div className="flex md:py-40 xl:py-60 2xl:py-80">
 			<div className='hidden md:block'>
-				<h1 className="text-4xl text-white font-starwars">Star Wars:<br />The Archive</h1>
+				<h1 className="text-4xl text-white font-starwars lg:text-6xl xl:text-7xl">Star Wars:<br />The Archive</h1>
 			</div>
 			<div className="h-full w-full md:absolute md:top-0 md:w-8/12 md:right-0 md:overflow-clip">
 				<Image src="/hero-luke-2.png" alt="Luke Skywalker" layout="responsive" width={750} height={858} priority />	
