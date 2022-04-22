f9ffca72f651bdc881ab85977a6cc83867458ad2
Liam <liam.winterton.dev@gmail.com>
2022-04-22 23:54:16 +0100

    Styled Species archive

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -1,17 +1,17 @@
 import Button from '../Button/Button'
 
 export default function Pagination(props) {
-	const { previous, next, count } = props
+	const { previous, next, count, path } = props
 
 	return(
 		<div>
 			<p className="mb-6">Results: {count}</p>
 			<div className="buttons">
 				{previous &&
-					<Button href={`${props.path}/${previous}`} text="Previous" />
+					<Button href={`${path}/${previous}`} text="Previous" />
 				}
 				{next !== false &&
-					<Button href={`${props.path}/${next}`} text="Next" />
+					<Button href={`${path}/${next}`} text="Next" />
 				}
 			</div>
 		</div>
--- components/Results/Results.js
+++ components/Results/Results.js
@@ -2,12 +2,12 @@ import ResultsList from './ResultsList'
 import Pagination from '../Pagination/Pagination'
 
 export default function Results(props) {
-	const { results, count, singlePath, imagePath, previous, next } = props
+	const { results, count, singlePath, archivePath, imagePath, previous, next } = props
 
 	return (
 		<div className="space-y-6 lg:space-y-12">
 			<ResultsList results={results} count={count} imagePath={imagePath} singlePath={singlePath} />
-			<Pagination path="/people" previous={previous} next={next} count={count} />
+			<Pagination path={archivePath} previous={previous} next={next} count={count} />
 		</div>
 	)
 }
